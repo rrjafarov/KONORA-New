6bbe82b2e6ac57a71d0a34b4046c9b5737621de7
Rafail Jafarov <105179488+rrjafarov@users.noreply.github.com>
2023-04-28 23:40:58 +0400

    newKonora

---
+++ Konora/assets/js/app.js
@@ -0,0 +1,79 @@
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", ()=>{
+    mobileNav = document.querySelector(".mobile-nav");
+    mobileNav.classList.add("active");
+})
+
+let xIcon = document.querySelector(".x-icon");
+xIcon.addEventListener("click", ()=>{
+  mobileNav.classList.remove("active");
+})
+
+
+// Event
+
+// ---------- open
+
+document.querySelector(".eventBtn").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "flex"
+})
+document.querySelector(".eventBtn").addEventListener("click", function(){
+  document.querySelector(".black-panel").classList.add("active");
+})
+
+// --------- close 
+
+document.querySelector(".popup-close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none"
+})
+
+document.querySelector(".popup-close").addEventListener("click", function(){
+  document.querySelector(".black-panel").classList.remove("active");
+})
+
+// Black Panel close
+
+
+document.querySelector(".black-panel").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none"
+})
+
+document.querySelector(".black-panel").addEventListener("click", function(){
+  document.querySelector(".black-panel").classList.remove("active");
+})
+
+
+
+
+
+// swiper partner -js
+var swiper = new Swiper(".swiperDoctors", {
+    slidesPerView: 4,
+    spaceBetween: 8,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 38,
+      },
+      640: {
+        slidesPerView: 2.5,
+        spaceBetween: 38,
+      },
+      768: {
+        slidesPerView: 3.5,
+        spaceBetween: 38,
+      },
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 38,
+      },
+    },
+  });
