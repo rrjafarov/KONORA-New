8142dd29e0de4ed2fbfe708cbd67dc7ca1c82856
Rafail Jafarov <105179488+rrjafarov@users.noreply.github.com>
2023-05-01 21:38:18 +0400

    newSliderGenerate

--- Konora/assets/js/app.js
+++ Konora/assets/js/app.js
@@ -1,46 +1,58 @@
 let hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", ()=>{
-    mobileNav = document.querySelector(".mobile-nav");
-    mobileNav.classList.add("active");
-})
+hamburger.addEventListener("click", () => {
+  mobileNav = document.querySelector(".mobile-nav");
+  mobileNav.classList.add("active");
+});
 
 let xIcon = document.querySelector(".x-icon");
-xIcon.addEventListener("click", ()=>{
+xIcon.addEventListener("click", () => {
   mobileNav.classList.remove("active");
-})
-
+});
 
 // Event
 
 // ---------- open
 
-document.querySelector(".eventBtn").addEventListener("click", function(){
-  document.querySelector(".popup").style.display = "flex"
-})
-document.querySelector(".eventBtn").addEventListener("click", function(){
+document.querySelector(".eventBtn").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "flex";
+});
+document.querySelector(".eventBtn").addEventListener("click", function () {
   document.querySelector(".black-panel").classList.add("active");
-})
+});
 
-// --------- close 
+// --------- close
 
-document.querySelector(".popup-close").addEventListener("click", function(){
-  document.querySelector(".popup").style.display = "none"
-})
+document.querySelector(".popup-close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
 
-document.querySelector(".popup-close").addEventListener("click", function(){
+document.querySelector(".popup-close").addEventListener("click", function () {
   document.querySelector(".black-panel").classList.remove("active");
-})
+});
 
 // Black Panel close
 
+document.querySelector(".black-panel").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
 
-document.querySelector(".black-panel").addEventListener("click", function(){
-  document.querySelector(".popup").style.display = "none"
-})
-
-document.querySelector(".black-panel").addEventListener("click", function(){
+document.querySelector(".black-panel").addEventListener("click", function () {
   document.querySelector(".black-panel").classList.remove("active");
-})
+});
+
+
+// slider hero
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+// slider hero
 
 
 
@@ -48,32 +60,36 @@ document.querySelector(".black-panel").addEventListener("click", function(){
 
 // swiper partner -js
 var swiper = new Swiper(".swiperDoctors", {
-    slidesPerView: 4,
-    spaceBetween: 8,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
+  slidesPerView: 4,
+  spaceBetween: 8,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 38,
     },
-    navigation: {
-      nextEl: ".swiper-next",
-      prevEl: ".swiper-prev",
+    640: {
+      slidesPerView: 2.5,
+      spaceBetween: 38,
     },
-    breakpoints: {
-      320: {
-        slidesPerView: 1.5,
-        spaceBetween: 38,
-      },
-      640: {
-        slidesPerView: 2.5,
-        spaceBetween: 38,
-      },
-      768: {
-        slidesPerView: 3.5,
-        spaceBetween: 38,
-      },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 38,
-      },
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 38,
     },
-  });
+    1024: {
+      slidesPerView: 3.5,
+      spaceBetween: 38,
+    },
+  },
+});
